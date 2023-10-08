785bbc2c37c06340e6b75917ab3cb275872303ec
Akhilesh Yethirajam <33382779+yethirak@users.noreply.github.com>
2023-10-08 16:19:39 +0800

    updated map

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "4bf745",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+	colors := make(map[string]string)
+	colors["white"] = "#ffffff"
+	colors["green"] = "#4bf745"
+	colors["red"] = "#ff0000"
+	// delete(colors, "white")
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
